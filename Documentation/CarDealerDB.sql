fa31a42e3bc0bceb82b48d23cb7eb23bab1a3fe6
David Santacoloma <adsi1409@hotmail.com>
2017-09-20 01:36:33 -0500

    Actualizar script de creación de tablas

--- Documentation/CarDealerDB.sql
+++ Documentation/CarDealerDB.sql
@@ -8,3 +8,29 @@ direccion varchar(255) NOT NULL,
 email varchar(255) NOT NULL,
 PRIMARY KEY (tipoIdentificacion, numeroIdentificacion)
 );
+
+CREATE TABLE tipoIdentificacion(
+codigo int PRIMARY KEY,
+descripcion varchar(50) NOT NULL
+);
+
+CREATE TABLE Vehiculos(
+matricula varchar(100) PRIMARY KEY,
+placa varchar(10) NOT NULL,
+marca varchar(100) NOT NULL,
+linea varchar(100) NOT NULL,
+modelo int NOT NULL,
+cilindraje double NOT NULL,
+color varchar(50) NOT NULL,
+tipoServicio varchar(50) NOT NULL,
+claseVehiculo varchar(50) NOT NULL,
+tipoCarroceria varchar(100) NOT NULL,
+tipoCombustible varchar(50) NOT NULL
+);
+
+ALTER TABLE Clientes 
+ADD CONSTRAINT Clientes_tipoIdentificacion 
+FOREIGN KEY (tipoIdentificacion) 
+REFERENCES tipoIdentificacion (codigo) 
+ON DELETE NO ACTION 
+ON UPDATE CASCADE;
