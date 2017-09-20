cf488d57ebcb2c60a9949879ee6d6facd3ea8601
David Santacoloma <adsi1409@hotmail.com>
2017-09-20 01:37:06 -0500

    Agregar modelos de tipo de identificación y vehículos

---
+++ CarDealer_SoftwareArchitectureLab/src/java/co/com/cardealer/entity/Vehiculo.java
@@ -0,0 +1,232 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.cardealer.entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author DAVID
+ */
+@Entity
+@Table(name = "vehiculos")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Vehiculo.findAll", query = "SELECT v FROM Vehiculo v")
+    , @NamedQuery(name = "Vehiculo.findByMatricula", query = "SELECT v FROM Vehiculo v WHERE v.matricula = :matricula")
+    , @NamedQuery(name = "Vehiculo.findByPlaca", query = "SELECT v FROM Vehiculo v WHERE v.placa = :placa")
+    , @NamedQuery(name = "Vehiculo.findByMarca", query = "SELECT v FROM Vehiculo v WHERE v.marca = :marca")
+    , @NamedQuery(name = "Vehiculo.findByLinea", query = "SELECT v FROM Vehiculo v WHERE v.linea = :linea")
+    , @NamedQuery(name = "Vehiculo.findByModelo", query = "SELECT v FROM Vehiculo v WHERE v.modelo = :modelo")
+    , @NamedQuery(name = "Vehiculo.findByCilindraje", query = "SELECT v FROM Vehiculo v WHERE v.cilindraje = :cilindraje")
+    , @NamedQuery(name = "Vehiculo.findByColor", query = "SELECT v FROM Vehiculo v WHERE v.color = :color")
+    , @NamedQuery(name = "Vehiculo.findByTipoServicio", query = "SELECT v FROM Vehiculo v WHERE v.tipoServicio = :tipoServicio")
+    , @NamedQuery(name = "Vehiculo.findByClaseVehiculo", query = "SELECT v FROM Vehiculo v WHERE v.claseVehiculo = :claseVehiculo")
+    , @NamedQuery(name = "Vehiculo.findByTipoCarroceria", query = "SELECT v FROM Vehiculo v WHERE v.tipoCarroceria = :tipoCarroceria")
+    , @NamedQuery(name = "Vehiculo.findByTipoCombustible", query = "SELECT v FROM Vehiculo v WHERE v.tipoCombustible = :tipoCombustible")})
+public class Vehiculo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "matricula")
+    private String matricula;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 10)
+    @Column(name = "placa")
+    private String placa;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "marca")
+    private String marca;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "linea")
+    private String linea;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "modelo")
+    private int modelo;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "cilindraje")
+    private double cilindraje;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "color")
+    private String color;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "tipoServicio")
+    private String tipoServicio;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "claseVehiculo")
+    private String claseVehiculo;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "tipoCarroceria")
+    private String tipoCarroceria;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "tipoCombustible")
+    private String tipoCombustible;
+
+    public Vehiculo() {
+    }
+
+    public Vehiculo(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public Vehiculo(String matricula, String placa, String marca, String linea, int modelo, double cilindraje, String color, String tipoServicio, String claseVehiculo, String tipoCarroceria, String tipoCombustible) {
+        this.matricula = matricula;
+        this.placa = placa;
+        this.marca = marca;
+        this.linea = linea;
+        this.modelo = modelo;
+        this.cilindraje = cilindraje;
+        this.color = color;
+        this.tipoServicio = tipoServicio;
+        this.claseVehiculo = claseVehiculo;
+        this.tipoCarroceria = tipoCarroceria;
+        this.tipoCombustible = tipoCombustible;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getLinea() {
+        return linea;
+    }
+
+    public void setLinea(String linea) {
+        this.linea = linea;
+    }
+
+    public int getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(int modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getCilindraje() {
+        return cilindraje;
+    }
+
+    public void setCilindraje(double cilindraje) {
+        this.cilindraje = cilindraje;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTipoServicio() {
+        return tipoServicio;
+    }
+
+    public void setTipoServicio(String tipoServicio) {
+        this.tipoServicio = tipoServicio;
+    }
+
+    public String getClaseVehiculo() {
+        return claseVehiculo;
+    }
+
+    public void setClaseVehiculo(String claseVehiculo) {
+        this.claseVehiculo = claseVehiculo;
+    }
+
+    public String getTipoCarroceria() {
+        return tipoCarroceria;
+    }
+
+    public void setTipoCarroceria(String tipoCarroceria) {
+        this.tipoCarroceria = tipoCarroceria;
+    }
+
+    public String getTipoCombustible() {
+        return tipoCombustible;
+    }
+
+    public void setTipoCombustible(String tipoCombustible) {
+        this.tipoCombustible = tipoCombustible;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (matricula != null ? matricula.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Vehiculo)) {
+            return false;
+        }
+        Vehiculo other = (Vehiculo) object;
+        if ((this.matricula == null && other.matricula != null) || (this.matricula != null && !this.matricula.equals(other.matricula))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "co.com.cardealer.entity.Vehiculo[ matricula=" + matricula + " ]";
+    }
+    
+}
