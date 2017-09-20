fd6b40453d7eac690f188e6e38f802e6a925ad73
David Santacoloma <adsi1409@hotmail.com>
2017-09-20 02:26:50 -0500

    Agregar modelo, session bean y servlet para las fotos

---
+++ CarDealer_SoftwareArchitectureLab/src/java/co/com/cardealer/ejb/FotoFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.cardealer.ejb;
+
+import co.com.cardealer.entity.Foto;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author DAVID
+ */
+@Stateless
+public class FotoFacade extends AbstractFacade<Foto> implements FotoFacadeLocal {
+
+    @PersistenceContext(unitName = "CarDealer_SoftwareArchitectureLabPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public FotoFacade() {
+        super(Foto.class);
+    }
+    
+}
---
+++ CarDealer_SoftwareArchitectureLab/src/java/co/com/cardealer/entity/Foto.java
@@ -0,0 +1,146 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.cardealer.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author DAVID
+ */
+@Entity
+@Table(name = "fotos")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Foto.findAll", query = "SELECT f FROM Foto f")
+    , @NamedQuery(name = "Foto.findByCodigo", query = "SELECT f FROM Foto f WHERE f.codigo = :codigo")
+    , @NamedQuery(name = "Foto.findByDescripcion", query = "SELECT f FROM Foto f WHERE f.descripcion = :descripcion")
+    , @NamedQuery(name = "Foto.findByVehiculo", query = "SELECT f FROM Foto f WHERE f.vehiculo = :vehiculo")
+    , @NamedQuery(name = "Foto.findByUrl", query = "SELECT f FROM Foto f WHERE f.url = :url")
+    , @NamedQuery(name = "Foto.findByFecha", query = "SELECT f FROM Foto f WHERE f.fecha = :fecha")})
+public class Foto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "codigo")
+    private Integer codigo;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "descripcion")
+    private String descripcion;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "vehiculo")
+    private String vehiculo;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "url")
+    private String url;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "fecha")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fecha;
+
+    public Foto() {
+    }
+
+    public Foto(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public Foto(Integer codigo, String descripcion, String vehiculo, String url, Date fecha) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.vehiculo = vehiculo;
+        this.url = url;
+        this.fecha = fecha;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(String vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (codigo != null ? codigo.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Foto)) {
+            return false;
+        }
+        Foto other = (Foto) object;
+        if ((this.codigo == null && other.codigo != null) || (this.codigo != null && !this.codigo.equals(other.codigo))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "co.com.cardealer.entity.Foto[ codigo=" + codigo + " ]";
+    }
+    
+}
