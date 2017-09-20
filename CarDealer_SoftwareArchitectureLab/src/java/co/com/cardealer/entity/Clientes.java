a3595e344d7b3bd36fb8dcc82b4ff59c60ff241e
David Santacoloma <adsi1409@hotmail.com>
2017-09-19 21:58:20 -0500

    Agregar entidad Clientes

---
+++ CarDealer_SoftwareArchitectureLab/src/java/co/com/cardealer/entity/Clientes.java
@@ -0,0 +1,161 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.cardealer.entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author DAVID
+ */
+@Entity
+@Table(name = "clientes")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Clientes.findAll", query = "SELECT c FROM Clientes c")
+    , @NamedQuery(name = "Clientes.findByTipoIdentificacion", query = "SELECT c FROM Clientes c WHERE c.clientesPK.tipoIdentificacion = :tipoIdentificacion")
+    , @NamedQuery(name = "Clientes.findByNumeroIdentificacion", query = "SELECT c FROM Clientes c WHERE c.clientesPK.numeroIdentificacion = :numeroIdentificacion")
+    , @NamedQuery(name = "Clientes.findByNombre", query = "SELECT c FROM Clientes c WHERE c.nombre = :nombre")
+    , @NamedQuery(name = "Clientes.findByApellido", query = "SELECT c FROM Clientes c WHERE c.apellido = :apellido")
+    , @NamedQuery(name = "Clientes.findByTelefono", query = "SELECT c FROM Clientes c WHERE c.telefono = :telefono")
+    , @NamedQuery(name = "Clientes.findByDireccion", query = "SELECT c FROM Clientes c WHERE c.direccion = :direccion")
+    , @NamedQuery(name = "Clientes.findByEmail", query = "SELECT c FROM Clientes c WHERE c.email = :email")})
+public class Clientes implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @EmbeddedId
+    protected ClientesPK clientesPK;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "nombre")
+    private String nombre;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "apellido")
+    private String apellido;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "telefono")
+    private String telefono;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "direccion")
+    private String direccion;
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "email")
+    private String email;
+
+    public Clientes() {
+    }
+
+    public Clientes(ClientesPK clientesPK) {
+        this.clientesPK = clientesPK;
+    }
+
+    public Clientes(ClientesPK clientesPK, String nombre, String apellido, String telefono, String direccion, String email) {
+        this.clientesPK = clientesPK;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.email = email;
+    }
+
+    public Clientes(int tipoIdentificacion, String numeroIdentificacion) {
+        this.clientesPK = new ClientesPK(tipoIdentificacion, numeroIdentificacion);
+    }
+
+    public ClientesPK getClientesPK() {
+        return clientesPK;
+    }
+
+    public void setClientesPK(ClientesPK clientesPK) {
+        this.clientesPK = clientesPK;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (clientesPK != null ? clientesPK.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Clientes)) {
+            return false;
+        }
+        Clientes other = (Clientes) object;
+        if ((this.clientesPK == null && other.clientesPK != null) || (this.clientesPK != null && !this.clientesPK.equals(other.clientesPK))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "co.com.cardealer.entity.Clientes[ clientesPK=" + clientesPK + " ]";
+    }
+    
+}
---
+++ CarDealer_SoftwareArchitectureLab/src/java/co/com/cardealer/entity/ClientesPK.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.cardealer.entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author DAVID
+ */
+@Embeddable
+public class ClientesPK implements Serializable {
+
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "tipoIdentificacion")
+    private int tipoIdentificacion;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "numeroIdentificacion")
+    private String numeroIdentificacion;
+
+    public ClientesPK() {
+    }
+
+    public ClientesPK(int tipoIdentificacion, String numeroIdentificacion) {
+        this.tipoIdentificacion = tipoIdentificacion;
+        this.numeroIdentificacion = numeroIdentificacion;
+    }
+
+    public int getTipoIdentificacion() {
+        return tipoIdentificacion;
+    }
+
+    public void setTipoIdentificacion(int tipoIdentificacion) {
+        this.tipoIdentificacion = tipoIdentificacion;
+    }
+
+    public String getNumeroIdentificacion() {
+        return numeroIdentificacion;
+    }
+
+    public void setNumeroIdentificacion(String numeroIdentificacion) {
+        this.numeroIdentificacion = numeroIdentificacion;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (int) tipoIdentificacion;
+        hash += (numeroIdentificacion != null ? numeroIdentificacion.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ClientesPK)) {
+            return false;
+        }
+        ClientesPK other = (ClientesPK) object;
+        if (this.tipoIdentificacion != other.tipoIdentificacion) {
+            return false;
+        }
+        if ((this.numeroIdentificacion == null && other.numeroIdentificacion != null) || (this.numeroIdentificacion != null && !this.numeroIdentificacion.equals(other.numeroIdentificacion))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "co.com.cardealer.entity.ClientesPK[ tipoIdentificacion=" + tipoIdentificacion + ", numeroIdentificacion=" + numeroIdentificacion + " ]";
+    }
+    
+}
